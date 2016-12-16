eba9f11df0a8e181baec836b9052f6dbd8ed3388
Benjamin Bakle <bbakle@gmail.com>
2016-12-15 23:15:46 -0500

    Added jquery to requireJS

--- Aptera.JS/js/app/main.js
+++ Aptera.JS/js/app/main.js
@@ -2,6 +2,7 @@ define(function (require) {
     // Load any app-specific modules
     // with a relative require call,
     // like:
+    var $ = require('jquery');
 
     var aptera = require('./aptera');
     var messages = require('./messages');
@@ -12,4 +13,14 @@ define(function (require) {
 
 
     print(messages.getHello());
+
+    $(document).ready(function () {
+        console.log($(".nav2"));
+        //:: DROPDOWN ITEMS TOGGLE ::/
+        $(".dropdown > a").click(function () {
+            $(this).siblings("ul").stop(false, false).slideToggle();
+            $(this).toggleClass("active");
+        });
+    })
+
 });
--- Aptera.JS/js/common.js
+++ Aptera.JS/js/common.js
@@ -6,7 +6,8 @@
 requirejs.config({
     baseUrl: 'js/lib',
     paths: {
-        app: '../app'
+        app: '../app',
+        jquery: '../lib/jquery-3.1.1'
     }
 });
 
